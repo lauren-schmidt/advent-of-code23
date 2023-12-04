bdd508f28776113d5010b8d9ca579fbed6e9b50d
Lauren <94632367+lauren-schmidt@users.noreply.github.com>
2023-12-04 13:05:41 -0500

    Day 1 Part 1 finished only

---
+++ Day One/day1part2.py
@@ -0,0 +1,22 @@
+from array import *
+import re
+
+# Read the file in line by line 
+
+calibration = open('day1.txt', 'r')
+lines = calibration.readlines()
+
+nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+res = []
+
+for line in lines:
+    # Find the first and last numeric OR letter digit in each line
+    match = re.findall(r'\d', line)
+    first = match[0][0]
+    last = match[-1]
+
+    # Combine those two numbers to find the value (ex: 3 + 8 = 38)
+    res.append(first + last)
+
+    # Convert the values to integers
+    res = list(map(int, res))
